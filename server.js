607127d56a17b2b938d0890f4f83c7e072f107f3
Andrew Kaplan <ac.kaplan11@berkeley.edu>
2022-03-01 13:02:34 -0800

    retrieve recipeName route, recipe details route

--- server.js
+++ server.js
@@ -1,5 +1,31 @@
 const bodyParser = require('body-parser')
 const express = require('express');
 const app = express();
+const PORT = 4000
+const data = require('./data.json')
 
+app.listen(PORT, () => console.log(`Express Server listening on Port ${PORT}`))
 
+app.get('/', (req, res) => {
+    res.send('Hello')
+})
+
+app.get('/recipes', (req, res) => {
+    const recipeNames = []
+    for (const recipe of data.recipes) {
+        recipeNames.push(recipe.name)
+    }
+    res.json({recipeNames: recipeNames})
+})
+
+app.get('/recipes/details/:recipeName', (req, res) => {
+    for (const recipe of data.recipes) {
+        if (recipe.name === req.params.recipeName) {       
+            return res.json({details: {
+                ingredients: recipe.ingredients,
+                numSteps: recipe.instructions.length
+            }})
+        }
+    }
+    res.json({})
+})
